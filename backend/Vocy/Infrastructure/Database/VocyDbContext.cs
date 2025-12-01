79a5cce74eca29fa2e2f80d65e45072cc80859d2
rybmks <rybalkomaks3@gmail.com>
2025-12-01 14:12:33 +0100

    add migrations draft

--- backend/Vocy/Infrastructure/Database/VocyDbContext.cs
+++ backend/Vocy/Infrastructure/Database/VocyDbContext.cs
@@ -3,12 +3,7 @@ namespace Infrastructure.Database;
 using Domain.User;
 using Microsoft.EntityFrameworkCore;
 
-public class VocyDbContext : DbContext
+public class VocyDbContext(DbContextOptions<VocyDbContext> options) : DbContext(options)
 {
-    public VocyDbContext(DbContextOptions<VocyDbContext> options)
-        : base(options)
-    {
-    }
-
     public DbSet<User> Users => Set<User>();
 }
