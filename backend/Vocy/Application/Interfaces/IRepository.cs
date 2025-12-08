513ffca6695bd78ee39e8c6cdae788bcf8038517
rybmks <rybalkomaks3@gmail.com>
2025-12-08 11:50:17 +0100

    add todos md

--- backend/Vocy/Application/Interfaces/IRepository.cs
+++ backend/Vocy/Application/Interfaces/IRepository.cs
@@ -4,5 +4,9 @@ namespace Application.User;
 
 public interface IRepository<T> where T : BaseIdentity
 {
+    // TODO: Change giud to BaseIdentity
+    Task<T> Get(Guid id);
+    Task Remove(Guid id);
+    Task Remove(T entity);
     Task Save(T entity);
 }
---
+++ backend/backend-todos.md
@@ -0,0 +1,6 @@
+## Backend todos
+
+* rework domain logic
+* add error handling
+* rework files and directories structure
+* add input data validation
