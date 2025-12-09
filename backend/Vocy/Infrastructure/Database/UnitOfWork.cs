6094f0092bf4d05d9f7971bed0eec08a83d58963
rybmks <rybalkomaks3@gmail.com>
2025-12-09 15:27:09 +0100

    Add repos cashing

--- backend/Vocy/Infrastructure/Database/UnitOfWork.cs
+++ backend/Vocy/Infrastructure/Database/UnitOfWork.cs
@@ -5,14 +5,20 @@ namespace Infrastructure.Database;
 
 public class UnitOfWork(VocyDbContext context) : IUnitOfWork
 {
+    private readonly Dictionary<Type, object> _repositories = new();
+
     public IRepository<T> GetRepository<T>() where T : BaseIdentity
     {
-        return new PgGenericRepository<T>(context);
+        var type = typeof(T);
+        if (_repositories.TryGetValue(type, out var repository)) return (IRepository<T>)repository;
+
+        repository = new PgGenericRepository<T>(context);
+        _repositories[type] = repository;
+
+
+        return (IRepository<T>)repository;
     }
 
 
-    public async Task SaveChangesAsync()
-    {
-        await context.SaveChangesAsync();
-    }
+    public async Task SaveChangesAsync() => await context.SaveChangesAsync();
 }
